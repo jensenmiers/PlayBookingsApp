3be761e44e5af669c5a629a082916abbf93734f1
Jensen Miers <jensenmiers@gmail.com>
2025-12-02 21:47:01 -0800

    deslop

--- supabase/migrations/20250115000000_add_venues_public_select_policy.sql
+++
@@ -1,8 +0,0 @@
--- Allow public SELECT access to active venues
--- This policy allows anyone (including unauthenticated users) to view venues where is_active = true
-CREATE POLICY "Public can view active venues"
-ON venues
-FOR SELECT
-TO public
-USING (is_active = true);
-
